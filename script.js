e80220754c4a17117eb6a829bd5276ea50f6d633
Yuleth <yulethaceves@gmail.com>
2023-03-17 13:24:59 -0500

    completed logic for generating password

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-function generatePassword (){
-var lenghtpass = parseInt(prompt("enter password lenght"))
-if(lenghtpass < 8 || lenghtpass > 128 || isNaN(lenghtpass)){
-  return "Invalid length entered, please enter any integer between 8-128"
-}
-var acceptUpper = confirm("Do you like to have uppercase letters in your password?")
-var acceptLower = confirm("Do you like to have lowercase letters in your password?")
-var acceptSymbols = confirm("Do you like to have symbols letters in your password?")
-var acceptNumbers = confirm("Do you like to have numbers letters in your password?")
-var acceptedCharacterList = []
-if(acceptLower){
-  acceptedCharacterList += "abcdefghijklmnopqrstuvwxyz"
-}
+function generatePassword() {
+  var lenghtpass = parseInt(prompt("enter password lenght"))
+  if (lenghtpass < 8 || lenghtpass > 128 || isNaN(lenghtpass)) {
+    return "Invalid length entered, please enter any integer between 8-128"
+  }
+  var acceptUpper = confirm("Do you like to have uppercase letters in your password?")
+  var acceptLower = confirm("Do you like to have lowercase letters in your password?")
+  var acceptSymbols = confirm("Do you like to have symbols letters in your password?")
+  var acceptNumbers = confirm("Do you like to have numbers letters in your password?")
+  var acceptedCharacterList = []
+  if (acceptLower) {
+    acceptedCharacterList += "abcdefghijklmnopqrstuvwxyz" //26
+  }
 
-if(acceptUpper){
-  acceptedCharacterList += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-}
+  if (acceptUpper) {
+    acceptedCharacterList += "ABCDEFGHIJKLMNOPQRSTUVWXYZ" //26
+  }
 
-if(acceptSymbols){
-  acceptedCharacterList += "!@#$%^&*()_-+="
-}
+  if (acceptSymbols) {
+    acceptedCharacterList += "!@#$%^&*()_-+=" //14
+  }
 
-if(acceptNumbers){
-  acceptedCharacterList += "1234567890"
-}
-console.log(acceptedCharacterList)
+  if (acceptNumbers) {
+    acceptedCharacterList += "1234567890" //10
+  }
+//  console.log(acceptedCharacterList)
+  var password = ""
+
+  for (let i = 0; i < lenghtpass; i++) { // i = i+1
+    var index = Math.floor(Math.random() * acceptedCharacterList.length)
+    password += acceptedCharacterList[index]
+  }
+  return password
 }
 // Write password to the #password input
 function writePassword() {
